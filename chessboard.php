edb610d453e11bd03aa5cd8ef8906bd2e8c1014f
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-04-02 19:02:55 +0300

    Adding comments

--- chessboard.php
+++ chessboard.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- CSS style defining the dimensions of the table and colors for the chessboard -->
     <style>
         table {
             width: 500px; 
@@ -18,20 +19,25 @@
     <title>Document</title>
 </head>
 <body>
+<!-- Table structure to display the chessboard -->
 <table border="1" style="">
     
     <?php 
+    // PHP script to generate rows and columns for the chessboard
     for($i=1 ; $i<=8 ; $i++)
     {
         echo "<tr>";
         for($j=1 ; $j<=8; $j++)
         { 
+            // Condition to determine if the cell should be blue-black or cream based on the sum of row and column indices
             if( ($i+$j)%2==0 )
             {
+                // Cream cell
                 echo "<td height=30px width=30px class='cream'> </td>";
             }
             else
             {
+                // Blue-black cell
                 echo "<td height=30px width=30px class='blue-black'> </td>";
             }
         }
