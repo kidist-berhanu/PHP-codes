ca3757075c9bd4428907785783d921cdb67216a6
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-04-04 20:40:39 +0300

    Update Pattern.php

--- Pattern.php
+++ Pattern.php
@@ -1,1 +1,15 @@
+<?php
+$height = 5; // Height of the triangle
 
+for ($i = 1; $i <= $height; $i++) {
+    // Printing spaces
+    for ($j = $height - $i; $j > 0; $j--) {
+        echo "&nbsp;";
+    }
+    // Printing asterisks
+    for ($k = 1; $k <= (2 * $i - 1); $k++) {
+        echo "*";
+    }
+    echo "<br>";
+}
+?>
