a492f5a023d2ca47bf18ae74796b7077d44cc41e
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-04-13 20:04:00 +0300

    include file

--- MVC/login.php
+++ MVC/login.php
@@ -1,1 +1,12 @@
+<?php
 
+include 'includes/include.inc.php';
+
+    if($_SERVER['REQUEST_METHOD']=== 'POST'){
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+        $view = new View();
+        $view->login($user,$pass);
+    }
+
+?>
