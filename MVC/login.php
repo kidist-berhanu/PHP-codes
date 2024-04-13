708682f528ff5778d9df70e9dde5bc499dcd30dd
Yabsra Yihenew <yabsrayabsra2222@gmail.com>
2024-04-13 20:04:26 +0300

    adding html

--- MVC/login.php
+++ MVC/login.php
@@ -10,3 +10,19 @@ include 'includes/include.inc.php';
     }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="username" id="">
+        <input type="text" name="password" id="">
+        <input type="submit" value="login">
+    </form>
+</body>
+</html>
