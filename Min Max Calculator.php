de3a55e5e8641d8ac404208a2c70c26b383732c1
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-04-21 22:50:26 +0300

    Adding styles

--- Min Max Calculator.php
+++ Min Max Calculator.php
@@ -55,6 +55,25 @@
 </head>
 <body>
 
-\
+<form method="post">
+    <label for="numbers">Enter numbers separated by commas:</label><br>
+    <input type="text" id="numbers" name="numbers" placeholder="e.g., 1, 2, 3, 4"><br><br>
+    <input type="submit" value="Calculate">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $numbers_input = $_POST["numbers"];
+    $numbers_array = explode(",", $numbers_input);
+    $numbers = array_map('intval', $numbers_array);
+
+    $min_value = min($numbers);
+    $max_value = max($numbers);
+
+    echo "<p class='result'>Minimum Value: $min_value</p>";
+    echo "<p class='result'>Maximum Value: $max_value</p>";
+}
+?>
+
 </body>
 </html>
