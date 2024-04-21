0c8b4b8a679701dc18e55d22f8c9f0b042a00d48
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-04-21 22:48:29 +0300

    Update Min Max Calculator.php

--- Min Max Calculator.php
+++ Min Max Calculator.php
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post">
+    <label for="numbers">Enter numbers separated by commas:</label><br>
+    <input type="text" id="numbers" name="numbers" placeholder="e.g., 1, 2, 3, 4"><br><br>
+    <input type="submit" value="Calculate">
+</form>
+
+
+
+</body>
+</html>
 
