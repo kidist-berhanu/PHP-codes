70f47b6bbe2fb0dd2fd9b8aa8b227c21b9061831
Kidist Berhanu <145928754+kidist-berhanu@users.noreply.github.com>
2024-04-21 22:48:42 +0300

    Update Min Max Calculator.php

--- Min Max Calculator.php
+++ Min Max Calculator.php
@@ -8,8 +8,19 @@
     <input type="submit" value="Calculate">
 </form>
 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $numbers_input = $_POST["numbers"];
+    $numbers_array = explode(",", $numbers_input);
+    $numbers = array_map('intval', $numbers_array);
 
+    $min_value = min($numbers);
+    $max_value = max($numbers);
+
+    echo "<p>Minimum Value: $min_value</p>";
+    echo "<p>Maximum Value: $max_value</p>";
+}
+?>
 
 </body>
 </html>
-
